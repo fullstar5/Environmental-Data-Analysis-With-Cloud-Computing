46306d7d804fabcdad00f2306e0153d8edfa3362
ellielliellie <162665859+ellielliellie@users.noreply.github.com>
2024-05-09 01:29:29 +1000

    new file:   DataProcessing/add_PHN_LGA.py 	modified:   Harvesters/healthdataharvester/healthdata_build.sh 	modified:   Harvesters/healthdataharvester/healthdata_harvester.py 	new file:   data/sorted_newData/add_PHN/PHN.ipynb 	new file:   data/sorted_newData/add_PHN/PM.json 	new file:   data/sorted_newData/add_PHN/add_PHN_LGA.py 	new file:   data/sorted_newData/add_PHN/healthdata_harvester.py 	new file:   data/sorted_newData/add_PHN/lga_site.json 	new file:   data/sorted_newData/add_PHN_LGA/PM.json 	new file:   data/sorted_newData/add_PHN_LGA/add_PHN_LGA.py 	new file:   data/sorted_newData/add_PHN_LGA/newPM.json

--- Harvesters/healthdataharvester/healthdata_build.sh
+++ Harvesters/healthdataharvester/healthdata_build.sh
@@ -6,24 +6,22 @@ USERNAME="elastic"
 PASSWORD="elastic"
 
 # File path
-FILE_PATH="/Users/yueyangwu/Desktop/CCC2/sorted_newData/add_PHN/PM.json"
+FILE_PATH="/Users/yueyangwu/Desktop/CCC2/sorted_newData/add_PHN_LGA/newPM.json"
 
-# Read the JSON file and iterate through each geographical region and period
-jq -c '.[] | to_entries[] | .value | to_entries[] | {key, value: .value} | .value | to_entries[] | {key, value: .value}' $FILE_PATH | while read -r entry
+# Read the JSON file and split into individual objects assuming the JSON array structure
+jq -c '.[]' $FILE_PATH | while read -r line
 do
   # Generate a unique ID for each document using uuidgen, which may require installing the uuid-runtime package
   UUID=$(uuidgen)
 
-  # Extract the key and region data
-  region=$(echo $entry | jq -r '.key')
-  data=$(echo $entry | jq -r '.value')
-
-  # Use curl to send each entry to Elasticsearch
+  # Use curl to send data to Elasticsearch
   curl -XPUT -k "$URL/$UUID" \
        --header 'Content-Type: application/json' \
        --user "$USERNAME:$PASSWORD" \
-       --data "$data" | jq '.'
+       --data "$line" | jq '.'
 
   # Optional: sleep to prevent overwhelming the server
   # sleep 1
 done
+
+
