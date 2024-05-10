d656a5ce32a2bfb6246b9497c071d90091950226
seulttom <734697407@qq.com>
2024-05-10 16:06:32 +1000

    deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2010-11-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2012-13-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2013-14-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2014-15-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2015-16-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/2016-17-data_sa_crime.csv 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/33ef22b5-d3d1-489f-82d1-e887ccbe8d3f.xlsx 	deleted:    data/crime_alcohol_drug_data/crime in south au(2010-2016)/www 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/aihw-phe-214.pdf 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-2-overview.xls 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-3-tobacco-RSEs-and-MOEs.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-3-tobacco.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-4-alcohol-RSEs-and-MOEs.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-4-alcohol.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-5-illicit-drugs-RSEs-and-MOEs.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/chapter-5-illicit-drugs.xlsx 	deleted:    data/crime_alcohol_drug_data/smoke_drink_drug(2001-2016)/www 	deleted:    data/crime_alcohol_drug_data/treatment/aihw-hse-207.pdf 	deleted:    data/crime_alcohol_drug_data/treatment/www 	modified:   selectdata/__pycache__/get_data.cpython-310.pyc 	modified:   selectdata/get_data.py 	modified:   selectdata/test_dd.ipynb

--- selectdata/get_data.py
+++ selectdata/get_data.py
@@ -1,6 +1,24 @@
+#get_data.py: the fuctions of searching for data via ES
+"""
+All functions must have 'es' when used, all results will be stored in a dict.
+                         ↓↓
+es = Elasticsearch(
+    'https://127.0.0.1:9200',
+    verify_certs=False,
+    basic_auth=('elastic', 'elastic'),
+    request_timeout=60,
+    ssl_show_warn=False
+    )
+"""
+
 
 def twitter(es, city=None, size=1000, language=None):
 
+    """
+    e.g.: results = twitter(es, city="Ballarat", size=5, language="de")
+    default size is 1000
+    Add whatever u want to search for (city or language or both).
+    """
     must_conditions = []
 
     if city:
@@ -17,7 +35,6 @@ def twitter(es, city=None, size=1000, language=None):
 
     query = {
         "size": size,
-        # "_source": ['language'],
         "query": query_part
     }
     
@@ -27,3 +44,148 @@ def twitter(es, city=None, size=1000, language=None):
     
     return hits
 
+
+def epa(es, start, end, avg=None, time=None, health_advice=None, city=None, health_parameter=None, size=1000):
+
+    """
+    start: start date, end: end date.
+                ↓
+                1
+    default size is 1000
+    Add whatever u want to search for.
+    """
+
+    must_conditions = []
+
+    index_names = ",".join([f"epa-air-quality-2024-05-{str(i).zfill(2)}" for i in range(start, end+1)])
+
+    if avg:
+        must_conditions.append({"match": {"averageValue": avg}})
+    if time:
+        must_conditions.append({"match": {"hour": time}})
+    if health_advice:
+        must_conditions.append({"match": {"healthAdvice": health_advice}})
+    if city:
+        must_conditions.append({"match": {"siteName": city}})
+    if health_parameter:
+        must_conditions.append({"match": {"healthParameter": health_parameter}})
+
+
+    if must_conditions:
+        query_part = {"bool": {"must": must_conditions}}
+    else:
+        query_part = {"match_all": {}}
+
+
+    query = {
+        "size": size,
+        "query": query_part
+    }
+    
+    response = es.search(index=index_names, body=query)
+
+    hits = response['hits']['hits']
+    
+    return hits
+
+
+def bom(es, start, end, air_temp=None, apparent_temp=None, cloud=None, cloud_type=None, delta_temp=None, dew_point=None, time=None, press=None, press_tend=None, rain_trace=None, vis_km=None, weather=None, wind_spd_kmh=None, size=1000):
+
+    """
+    start: start date, end: end date.
+                ↓
+                3
+    default size is 1000
+    Add whatever u want to search for.
+    """
+
+    must_conditions = []
+
+    index_names = ",".join([f"bom-weather-202405{str(i).zfill(2)}" for i in range(start, end+1)])
+
+    if air_temp:
+        must_conditions.append({"match": {"air_temperature": air_temp}})
+    if apparent_temp:
+        must_conditions.append({"match": {"apparent_temperature": apparent_temp}})
+    if cloud:
+        must_conditions.append({"match": {"cloud": cloud}})
+    if cloud_type:
+        must_conditions.append({"match": {"cloud_type": cloud_type}})
+    if delta_temp:
+        must_conditions.append({"match": {"delta_temperature": delta_temp}})
+    if dew_point:
+        must_conditions.append({"match": {"dew_point": dew_point}})
+    if time:
+        must_conditions.append({"match": {"time": time}})
+    if press:
+        must_conditions.append({"match": {"press": press}})
+    if press_tend:
+        must_conditions.append({"match": {"press_tend": press_tend}})
+    if rain_trace:
+        must_conditions.append({"match": {"rain_trace": rain_trace}})
+    if vis_km:
+        must_conditions.append({"match": {"vis_km": vis_km}})
+    if weather:
+        must_conditions.append({"match": {"weather": weather}})
+    if wind_spd_kmh:
+        must_conditions.append({"match": {"wind_spd_kmh": wind_spd_kmh}})
+
+    if must_conditions:
+        query_part = {"bool": {"must": must_conditions}}
+    else:
+        query_part = {"match_all": {}}
+
+
+    query = {
+        "size": size,
+        "query": query_part
+    }
+    
+    response = es.search(index=index_names, body=query)
+
+    hits = response['hits']['hits']
+    
+    return hits
+
+
+def health(es, asr=None, disease=None, lga=None, num=None, period=None, phn=None, sr=None, size=1000):
+
+    """
+    default size is 1000
+    Add whatever u want to search for.
+    """
+
+    must_conditions = []
+
+    if asr:
+        must_conditions.append({"match": {"ASR": asr}})
+    if disease:
+        must_conditions.append({"match": {"Disease": disease}})
+    if lga:
+        must_conditions.append({"match": {"LGA": lga}})
+    if num:
+        must_conditions.append({"match": {"NUM": num}})
+    if period:
+        must_conditions.append({"match": {"Period": period}})
+    if phn:
+        must_conditions.append({"match": {"PHN": phn}})
+    if sr:
+        must_conditions.append({"match": {"SR": sr}})
+
+
+    if must_conditions:
+        query_part = {"bool": {"must": must_conditions}}
+    else:
+        query_part = {"match_all": {}}
+
+
+    query = {
+        "size": size,
+        "query": query_part
+    }
+    
+    response = es.search(index="health_data", body=query)
+
+    hits = response['hits']['hits']
+    
+    return hits
